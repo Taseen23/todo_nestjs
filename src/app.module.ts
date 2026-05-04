65ae4f90b159b26b82510d35e8369ba50360bfd5
Taseen23 <taseenalam13@gmail.com>
2026-05-04 15:23:51 +0600

    Setup orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreateModule } from './create/create.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from './ormconfig';
 
 @Module({
-  imports: [CreateModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), CreateModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'devuser',
+  password: '1234',
+  database: 'todo',
+};
+
+export default config;
